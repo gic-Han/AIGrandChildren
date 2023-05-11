1808b5829b32b11ed4e4b4914a5f5aaf24cce54d
gic_Han <gichan4444@gmail.com>
2023-05-11 17:33:19 +0900

    setting

--- app/src/main/java/com/example/aigrandchildren/MainActivity.kt
+++ app/src/main/java/com/example/aigrandchildren/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.aigrandchildren
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +10,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun onDeleteButtonClick(v: View) {
+    fun onDeleteButtonClick() {
         val intent = Intent(this@MainActivity,FilelistActivity::class.java)
         startActivity(intent)
     }
 
-    fun onCallButtonClick(v: View) {
+    fun onCallButtonClick() {
         val intent = Intent(Intent.ACTION_DIAL)
         startActivity(intent)
     }
