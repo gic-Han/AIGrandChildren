62c8f80e82717f34a1b1525e1d996e79334c0b2e
gic_Han <gichan4444@gmail.com>
2023-05-28 22:38:18 +0900

    setting

--- app/src/main/java/com/example/aigrandchildren/MainActivity.kt
+++ app/src/main/java/com/example/aigrandchildren/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.aigrandchildren
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import android.Manifest
--- app/src/main/java/com/example/aigrandchildren/TTSsetting.kt
+++ app/src/main/java/com/example/aigrandchildren/TTSsetting.kt
@@ -2,7 +2,6 @@ package com.example.aigrandchildren
 
 import android.content.Context
 import android.speech.tts.TextToSpeech
-import android.util.Log
 import java.util.*
 
 object TTSsetting{
