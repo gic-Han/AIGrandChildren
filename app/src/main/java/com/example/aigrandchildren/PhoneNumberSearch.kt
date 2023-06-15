d3033ad1c0e306873dd8b3d6a8844c4456917c5e
gic_Han <gichan4444@gmail.com>
2023-06-16 00:40:13 +0900

    gicHan

--- app/src/main/java/com/example/aigrandchildren/PhoneNumberSearch.kt
+++ app/src/main/java/com/example/aigrandchildren/PhoneNumberSearch.kt
@@ -7,11 +7,12 @@ import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+
 class PhoneNumberSearch: AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.dialog)
 
         val OKbtn = findViewById<Button>(R.id.OK_button)
@@ -20,7 +21,7 @@ class PhoneNumberSearch: AppCompatActivity(){
         val phoneNumber = bundle?.getString("phoneNumber")
         Log.d("call", "전화번호: $phoneNumber")
 
-        if (searchPhoneNumber(phoneNumber)) {
+        if (searchPhoneNumber(phoneNumber!!)) {
             Log.d("dialog", "액티비티 OFF")
             finish()
         } else {
@@ -31,21 +32,40 @@ class PhoneNumberSearch: AppCompatActivity(){
         }
     }
 
-    private fun searchPhoneNumber(phoneNumber: String?): Boolean {
+    private fun searchPhoneNumber(Number: String): Boolean {
+        val phoneNumber = convertPhoneNumber(Number)
+        Log.d("dialog", "번호 형식 변경 $phoneNumber")
+
         val contactscontractUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
         val projection = arrayOf(ContactsContract.CommonDataKinds.Phone.NUMBER)
         val selection = "${ContactsContract.CommonDataKinds.Phone.NUMBER} = ?"
         val selectionArgs = arrayOf(phoneNumber)
-
         Log.d("dialog", "번호 찾는 중 ${selectionArgs[0]}")
         contentResolver.query(contactscontractUri, projection, selection, selectionArgs, null)?.use { cursor ->
             if (cursor.count > 0) {
                 Log.d("dialog", "번호 있음 ${cursor.count}")
+                cursor.moveToFirst()
+                Log.d("dialog", cursor.getString(0))
                 return true
             }
         }
 
         Log.d("dialog", "번호 없음")
         return false
+    }
+    private fun convertPhoneNumber(phone: String) : String{
+        return if(phone.startsWith("02")){
+            if(phone.length < 10){
+                phone.substring(0, 2) + "-" + phone.substring(2, 5) + "-" + phone.substring(5)
+            } else {
+                phone.substring(0, 2) + "-" + phone.substring(2, 6) + "-" + phone.substring(6)
+            }
+        } else {
+            if(phone.length < 11){
+                phone.substring(0, 3) + "-" + phone.substring(3, 6) + "-" + phone.substring(6)
+            } else {
+                phone.substring(0, 3) + "-" + phone.substring(3, 7) + "-" + phone.substring(7)
+            }
+        }
     }
 }
